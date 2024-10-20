686ce123cd0a9aea9b1e3bf106c224b628d0f7af
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-20 12:19:36 +0100

    task 6 again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,17 @@
-import signUpUser from "./4-user-promise"
-import uploadPhoto from "./5-photo-reject"
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const signup = signUpUser(firstName, lastName);
-    const upload = uploadPhoto(fileName);
-    const promise = Promise.allSettled([signup, upload])
-    return promise
-        .then((results) => {
-            return results.map(result => ({
-                status: result.status,
-                message: result.status === 'fulfilled' ? result.value : result.reason
-            }));
-        });
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ];
+  
+  return Promise.allSettled(promises)
+    .then((results) => {
+      return results.map(result => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+      }));
+    });
 }
