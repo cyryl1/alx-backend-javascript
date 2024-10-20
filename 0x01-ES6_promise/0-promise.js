4e69acd87179f81b0bc78c186a98b4b66bd5e8ab
Praise Aribisala <aribisalapraise12@gmail.com>
2024-10-20 10:24:51 +0100

    task 0 again

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,12 +2,10 @@ export default function getResponseFromAPI()
 {
     return new Promise((resolve, reject) => {
         let success = true;
-        setTimeout(() => {
-            if (success) { // Simulate API success
-                resolve({ status: 200, body: 'API response' });
-            } else {
-                reject(new Error('API request failed'));
-            }
-       }, 1000);
+        if (success) { // Simulate API success
+            resolve({ status: 200, body: 'API response' });
+        } else {
+            reject(new Error('API request failed'));
+        }
     });
 }
