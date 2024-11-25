40ac61f975d262d434e98f5310ffa53190b365ac
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-25 17:03:44 +0100

    Task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,7 +5,7 @@ function countStudents(path) {
         return new Promise((resolve, reject) => {
                 fs.readFile(path, 'utf8', (err, data) => {
 			if (err) {
-				reject(new Error('Cannot load the database'));
+				throw new Error('Cannot load the database');
 				return;
 			}
 
