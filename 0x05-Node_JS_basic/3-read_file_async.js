1f63d14353179f5f966382adf0356bea44f26b81
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-25 16:58:03 +0100

    Task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+function countStudents(path) {
+        return new Promise((resolve, reject) => {
+                fs.readFile(path, 'utf8', (err, data) => {
+			if (err) {
+				reject(new Error('Cannot load the database'));
+				return;
+			}
+
+			parse(data, { delimiter: ',', from_line: 2 }, (err, records) => {
+				if (err) {
+					reject(new Error('Cannot parse the CSV data'));
+					return;
+				}
+				const fields = {};
+
+				const validRows = records.filter(row => row.length > 0 && row[0] !== '');
+				validRows.forEach(row => {
+					const firstName = row[0];
+					const field = row[3];
+
+					if (!fields[field]) {
+						fields[field] = [];
+					}
+					fields[field].push(firstName);
+				});
+
+				const totalStudents = validRows.length;
+				console.log(`Number of students: ${totalStudents}`);
+
+				Object.entries(fields).forEach(([field, students]) => {
+					console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+				});
+
+				resolve();
+			});
+		});
+	});
+}
+
+module.exports = countStudents;
+
