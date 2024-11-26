6cc4e938290f59f0eea8e81722d07f8f57808f12
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-26 15:48:46 +0100

    Commit

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,14 @@
+class AppController {
+	/**
+	 * Handles the root route and sends a welcome message.\
+	 * @param {Object} req - The request object.
+	 * @param {Object} res - The response object.
+	 */
+
+	static getHomepage(req, res) {
+		res.status(200).send('Hello Holberton School!');
+	}
+}
+
+export default AppController;
+
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * Reads a database CSV file and organizes student first names by fields.
+ * @param {string} filePath - Path to the database file.
+ * @returns {Promise<Object>} A promise that resolves to an object with fields as 
+ * keys and arrays of student first names as values.
+*/
+
+export const readDatabase = (filePath) => {
+	return new Promise((resolve, reject) => {
+		const filePath = path.resolve(__dirname, '..', filePath);
+		fs.readFile(path.resolve(filePath), 'utf-8', (err, data) => {
+			if (err) reject(err);
+			const students = JSON.parse(data);
+			const fields = {};
+
+			students.forEach((student) => {
+				const { firstname, field } = student;
+				if (!fields[field]) {
+					fields[field] = [];
+				}
+				fields[field].push(firstname);
+			});
+			resolve(fields);
+		});
+	});
+};
+
