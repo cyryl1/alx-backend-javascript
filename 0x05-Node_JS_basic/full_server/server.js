82da1f85b8a4b1f75659553043a3a19f190cf9b2
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-26 16:04:01 +0100

    commit

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,27 @@
+// import express from 'express';
+// import routes from './routes/index';
+
+// const app = express();
+// const port = 1245;
+
+// app.use(routes);
+
+// app.listen(port);
+
+// export default app;
+
 import express from 'express';
-import routes from './routes/index';
+import routes from './routes/index.js';
 
 const app = express();
 const port = 1245;
 
-app.use(routes);
+// Use the routes
+app.use('/', routes);
 
-app.listen(port);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 export default app;
