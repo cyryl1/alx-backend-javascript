432c5be8df0d1b0ac2a9fa3dc773b547c154d12d
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-26 16:46:42 +0100

    commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,18 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+// Prompt the user to enter their name
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-
-process.stdin.on('data', (input) => {
-	const name = input.toString().trim();
-	console.log(`Your name is: ${name}`);
-
+// Listen for user input on standard input
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  
+  // If name is provided, display it
+  if (name !== null) {
+    const trimmedName = name.toString().trim();
+    process.stdout.write(`Your name is: ${trimmedName}\n`);
+  }
 });
 
+// Handle the end of input (Ctrl+D or Ctrl+C)
 process.stdin.on('end', () => {
-	console.log("This important software is now closing");
+  process.stdout.write('This important software is now closing\n');
 });
